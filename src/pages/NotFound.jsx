ba454666742c7f94c020623cd064eb4339d67b9d
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2022-07-01 21:01:17 +0500

    Basic UI is done!

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,21 @@
-
+import {FaHome} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className="hero">
+        <div className='text-center hero-center'>
+            <div className='max-w-2xl'>
+                <h1 className='font-bold mb-8 text-5xl'>No Resource</h1>
+                <p className='text-xl text-gray-300 mb-8'>404 - Page Not Found!</p>
+                <Link to='/' className='btn btn-primary btn-md'>
+                    <FaHome className='mr-2'/>
+                    Go Home
+                </Link>
+            </div>
+
+        </div>
+    </div>
   )
 }
 
