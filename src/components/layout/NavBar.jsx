e18e7e1231fa6ec4487bb8c22de28900e39cfc7a
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2022-07-06 20:03:32 +0500

    Icon is made clickable.

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -8,7 +8,7 @@ function NavBar(props) {
     <nav className="navbar shadow-lg bg-neutral mb-12 text-neutral-content">
         <div className='container mx-auto'> 
             <div className='flex-none mx-2 px-2'>
-                <FaGithub className='inline pr-2 text-3xl'/>
+                <Link to='/' ><FaGithub className='inline pr-2 text-3xl'/></Link>
                 <Link to='/' className='text-lg font-bold align-middle' >{props.title}</Link>
             </div>
             <div className='px-2 mx-2 flex-1'>
