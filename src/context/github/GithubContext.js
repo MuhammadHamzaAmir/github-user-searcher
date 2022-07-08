f302814339fc172ce3176288edb16f37ecc4b28e
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2022-07-08 19:44:55 +0500

    Limit repos info to 10 with latest ones only

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -75,7 +75,12 @@ export const GithubProvider = ({ children }) => {
 
         setLoading();
 
-        const response = await fetch(`${GITHUB_URL}/users/${login}/repos`,{
+        const params = new URLSearchParams({
+            sort: `created`,
+            per_page : 10,
+        })
+
+        const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`,{
             headers: {
                 Authorization: `token ${GITHUB_TOKEN}`
             }
